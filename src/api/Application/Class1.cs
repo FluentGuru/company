8355ad7f9dcb6b44bd715664a248444b9414cec5
Paulo Tavares <paulo.enmanuel@hotmail.com>
2020-07-17 13:21:47 -0400

    Adding propject structure

---
+++ src/api/Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Company.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/api/Messages/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Company.Messages
+{
+    public class Class1
+    {
+    }
+}
